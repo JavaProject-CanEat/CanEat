a304e9a9ee04f2c2b20bedbd3b76daff6529dfd8
LJM <banjakk@naver.com>
2021-11-03 11:48:55 +0900

    ocr이미지

--- app/src/main/java/com/example/caneat/MainActivity.java
+++ app/src/main/java/com/example/caneat/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.caneat;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,9 +16,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.gson.Gson;
@@ -27,6 +35,7 @@ import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Objects;
 
 
@@ -37,18 +46,51 @@ public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     FirebaseFunctions mFunctions = FirebaseFunctions.getInstance();// ocr 함수 초기화를 위한 변수 설정// 변수 mfunctions를 이용해 functions 초기화
 
+
     static final int REQUEST_IMAGE_CAPTURE = 1; //카메라 변수
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
+        ImageView img_test = findViewById(R.id.image);
 
-        imageview1 = findViewById(R.id.image);
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://javaproject-caneat.appspot.com/");
+        StorageReference storageRef = storage.getReference();
+        storageRef.child("imagefile/20211016_231825.jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //이미지 로드 성공시
+
+                Glide.with(getApplicationContext())
+                        .load(uri)
+                        .into(img_test);
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                //이미지 로드 실패시
+                Toast.makeText(getApplicationContext(), "실패", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        //imageview1 = findViewById(R.id.image);
         textview1 = findViewById(R.id.text1);
+
+
 
         Button My_info = (Button) findViewById(R.id.myinfo);
         My_info.setOnClickListener(new View.OnClickListener() {
@@ -56,13 +98,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent My_info = new Intent(getApplicationContext(),Myinfo_activity.class);
                 startActivity(My_info);
-
             }
         });
 
 
 
     }
+
+
 
     public void showCameraBtn(View view){ //카메라 버튼
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -75,10 +118,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) { //이미지 bitmap로 가져오기  (android developer)
         //(android developer)bitmap으로 가져오기
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            bitmap = (Bitmap) extras.get("data");
-            imageview1.setImageBitmap(bitmap);
+       // if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Bundle extras = data.getExtras();
+            //bitmap = (Bitmap) extras.get("data");
+           // imageview1.setImageBitmap(bitmap);
 
             //파이어베이스 cloud vision api 사용 코드
             // Scale down bitmap size
@@ -121,40 +164,20 @@ public class MainActivity extends AppCompatActivity {
                             toast.show();
                         } else {
                             //인식된 텍스트 문자열로 가져오기
+                            Toast toast = Toast.makeText(MainActivity.this,"이미지 있음", Toast.LENGTH_SHORT);
+                            toast.show();
                             JsonObject annotation = Objects.requireNonNull(task.getResult()).getAsJsonArray().get(0).getAsJsonObject().get("fullTextAnnotation").getAsJsonObject();
                             System.out.format("%nComplete annotation:%n");
-                            System.out.format("%s%n", annotation.get("text").getAsString());
-                            for (JsonElement page : annotation.get("pages").getAsJsonArray()) {
-                                StringBuilder pageText = new StringBuilder();
-                                for (JsonElement block : page.getAsJsonObject().get("blocks").getAsJsonArray()) {
-                                    StringBuilder blockText = new StringBuilder();
-                                    for (JsonElement para : block.getAsJsonObject().get("paragraphs").getAsJsonArray()) {
-                                        StringBuilder paraText = new StringBuilder();
-                                        for (JsonElement word : para.getAsJsonObject().get("words").getAsJsonArray()) {
-                                            StringBuilder wordText = new StringBuilder();
-                                            for (JsonElement symbol : word.getAsJsonObject().get("symbols").getAsJsonArray()) {
-                                                wordText.append(symbol.getAsJsonObject().get("text").getAsString());
-                                                System.out.format("Symbol text: %s (confidence: %f)%n", symbol.getAsJsonObject().get("text").getAsString(), symbol.getAsJsonObject().get("confidence").getAsFloat());
-                                            }
-                                            System.out.format("Word text: %s (confidence: %f)%n%n", wordText.toString(), word.getAsJsonObject().get("confidence").getAsFloat());
-                                            System.out.format("Word bounding box: %s%n", word.getAsJsonObject().get("boundingBox"));
-                                            paraText.append(wordText.toString()).append(" ");
-                                        }
-                                        System.out.format("%nParagraph:%n%s%n", paraText);
-                                        System.out.format("Paragraph bounding box: %s%n", para.getAsJsonObject().get("boundingBox"));
-                                        System.out.format("Paragraph Confidence: %f%n", para.getAsJsonObject().get("confidence").getAsFloat());
-                                        blockText.append(paraText);
-                                    }
-                                    pageText.append(blockText);
-                                }
-                                textview1.setText(pageText);
-                            }
+                            String ak = String.format("%s%n", annotation.get("text").getAsString());
+                            Log.d("로그확인", "ㅁㄴㅇㄹ");
+                            textview1.setText(ak);
                         }
+
                     });
         }
 
 
-    }
+
 
     private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) { // 이미지축소
         int originalWidth = bitmap.getWidth();
@@ -175,14 +198,20 @@ public class MainActivity extends AppCompatActivity {
         return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
 
-    public Task<JsonElement> annotateImage(String requestJson) { //함수 호출을 위한 메서드 정의
+    private Task<JsonElement> annotateImage(String requestJson) {
         return mFunctions
                 .getHttpsCallable("annotateImage")
                 .call(requestJson)
-                .continueWith(task -> JsonParser.parseString(new Gson().toJson(Objects.requireNonNull(task.getResult()).getData())));
-
+                .continueWith(new Continuation<HttpsCallableResult, JsonElement>() {
+                    @Override
+                    public JsonElement then(@NonNull Task<HttpsCallableResult> task) {
+                        // This continuation runs on either success or failure, but if the task
+                        // has failed then getResult() will throw an Exception which will be
+                        // propagated down.
+                        return JsonParser.parseString(new Gson().toJson(task.getResult().getData()));
+                    }
+                });
     }
-
 
 
 }
